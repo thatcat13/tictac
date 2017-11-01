64ea0ff2388c85c77425af27d25be0eedea70767
Cat Janowitz and Sam Kissee <sam@>
2017-11-01 14:26:39 -0700

    initial file set up

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+function Player(gamePiece){
+  this.piece = gamePiece
+}
+var playerOne = new Player("X");
+var playerTwo = new Player("O");
+
+
+
+testPlayer.mark(); // returns "X"
+
+var board = new Board();
+var testSpace = board.find(1, 2); // board.find(1,2) returns a Space object
+
+testSpace.xCoordinate(); // returns 1
+testSpace.yCoordinate(); // returns 2
+
+testSpace.mark(testPlayer);
+testSpace.markedBy(); // returns testPlayer or "X"
+
+board.gameOver(); // returns a boolean
