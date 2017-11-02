9220e704b234aeb3cdb4918aaeaec215e26ee367
Cat Janowitz and Sam Kissee <sam@>
2017-11-02 14:20:03 -0700

    experiment with for loop to assess winning row or column

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
-function Player(mark){
-  this.mark = mark;
-  this.mark=(this.mark === "X") ? "O": "X";
+function Player(id){
+  this.id = id;
+
 }
 var playerOne = new Player("X");
 var playerTwo = new Player("O");
 // function Board(space)
 
-
-
-function Space(x,y,value){
+function Space(x,y, value){
   this.x = x
   this.y = y
-  this.value= value;
+  this.value = value;
 }
+
+function Board(space, value) {
+  this.space = space;
+  this.value = value
+}
+var columnWin = [x= 1 || 2 || 3, y = "", value = "X"]
+
+var rowWin = [x= "", y =  1 || 2 || 3, value = "X"]
+
+var diagonal1 =([x =  1, y= 1, value= "X" || "O"] && [x= 2, y= 2, value= "X" || "O"] && [x= 3, y= 3, value= "X" || "O"]);
+
+//
+// var diagonal2 =({x: 3 y:3 value: X || O},
+//                {x: 2 y:2 value: X || O},
+//                {x: 1 y:1 value: X || O});
+var board = new Board();
 // var spaceOne = new Space()
 // var board = new Board();
 //
@@ -33,6 +47,8 @@ function Space(x,y,value){
 //this.value= "X";
 //this.value=(this.value === "X") ? "O": "X";
 var coordinates = new Array();
+
+
 
 $(document).ready(function(){
   //Board Click Function
@@ -57,10 +73,20 @@ $(document).ready(function(){
         var yCoor = this.parentNode.rowIndex +1;
         var cellValue = $(this).text();
         coordinates.push(new Space(xCoor, yCoor, cellValue));
+        // var board = {space:[xCoor, yCoor], cellValue};
         console.log(coordinates);
         // alert("cell: " + xCoor+ " / row: " + yCoor );
+          for (var i = 0; i < coordinates.length; i++){
+          if ((x[i])  && (y[i]) && (cellValue) === columnWin) {
+            alert("Player " + cellValue + "wins!")
+          } else if ((x[i])  && (y[i]) && (cellValue) === rowWin)
+            alert("Player " + cellValue + "wins!")
+        }
+      }
       };
-    // alert(playerOne.mark);
 
-  }
-});
+
+    //   for (var y = 0; y < coordinates.length; y++)
+    // // alert(playerOne.mark);
+
+  })
